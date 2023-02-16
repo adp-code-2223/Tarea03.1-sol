e1c95fe9749b4779bd9520805ed72a8536588769
María <frm@fernandowirtz.com>
2023-02-16 12:53:47 +0100

    Create dept dialog opens

---
+++ src/main/CreateNewDeptDialog.java
@@ -0,0 +1,116 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import modelo.Departamento;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.GraphicsConfiguration;
+import java.awt.Window;
+
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CreateNewDeptDialog extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textFieldUbicacion;
+	private JTextField textFieldNombreDept;
+	private JButton okButton;
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		try {
+//			CreateNewDeptDialog dialog = new CreateNewDeptDialog();
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public void initComponents() {
+		setTitle("Nuevo departamento");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+
+		JLabel lblDeptName = new JLabel("Nombre departamento");
+		lblDeptName.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDeptName.setBounds(39, 34, 140, 24);
+		contentPanel.add(lblDeptName);
+
+		textFieldUbicacion = new JTextField();
+		textFieldUbicacion.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textFieldUbicacion.setBounds(227, 82, 197, 23);
+		contentPanel.add(textFieldUbicacion);
+		textFieldUbicacion.setColumns(10);
+
+		JLabel lblDeptLocation = new JLabel("Ubicación");
+		lblDeptLocation.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblDeptLocation.setBounds(39, 82, 140, 24);
+		contentPanel.add(lblDeptLocation);
+
+		textFieldNombreDept = new JTextField();
+		textFieldNombreDept.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textFieldNombreDept.setColumns(10);
+		textFieldNombreDept.setBounds(227, 40, 197, 23);
+		contentPanel.add(textFieldNombreDept);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+		okButton = new JButton("Crear");
+
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setActionCommand("Cancel");
+		buttonPane.add(cancelButton);
+
+		ActionListener crearBtnActionListener = new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				if (!(textFieldUbicacion.getText().trim().equals(""))
+						&& !(textFieldNombreDept.getText().trim().equals(""))) {
+					Departamento newDept = new Departamento();
+					newDept.setDname(textFieldNombreDept.getText().trim());
+					newDept.setLoc(textFieldUbicacion.getText().trim());
+				}
+			}
+		};
+
+		this.okButton.addActionListener(crearBtnActionListener);
+
+	}
+
+	public CreateNewDeptDialog(Window owner, ModalityType modalityType) {
+		super(owner, modalityType);
+		initComponents();
+		this.setLocationRelativeTo(owner);
+	}
+	
+	
+}
--- src/main/TxWindow.java
+++ src/main/TxWindow.java
@@ -20,10 +20,13 @@ import javax.swing.JLabel;
 import java.awt.Font;
 
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dialog;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.List;
@@ -31,17 +34,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 import java.awt.Rectangle;
 import java.awt.Dimension;
 
 public class TxWindow extends JFrame {
 
 	private JPanel contentPane;
-	
+
 	private JTextArea mensajes_text_Area;
-	private JList JListAllDepts; 
-	
-	
+	private JList JListAllDepts;
+
 	private IDepartamentoServicio departamentoServicio;
 
 	/**
@@ -66,7 +70,7 @@ public class TxWindow extends JFrame {
 	public TxWindow() {
 
 		departamentoServicio = new DepartamentoServicio();
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 847, 772);
 		contentPane = new JPanel();
@@ -90,59 +94,54 @@ public class TxWindow extends JFrame {
 		mensajes_text_Area.setText("Panel de mensajes");
 		mensajes_text_Area.setForeground(new Color(255, 0, 0));
 		mensajes_text_Area.setFont(new Font("Monospaced", Font.PLAIN, 13));
-		
+
 		JButton btnShowAllDepts = new JButton("Mostrar departamentos");
-		
+
 		btnShowAllDepts.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnShowAllDepts.setBounds(50, 37, 208, 36);
 		panel.add(btnShowAllDepts);
-		//panel.add(JListAllDepts);
-		
+		// panel.add(JListAllDepts);
+
 		JScrollPane scrollPanel_in_JlistAllDepts = new JScrollPane();
-		//scrollPane_1.setBounds(284, 237, 505, -200);
+		// scrollPane_1.setBounds(284, 237, 505, -200);
 		panel.add(scrollPanel_in_JlistAllDepts);
 
-		 JListAllDepts = new JList();
-		 panel.add(JListAllDepts);
-		 
-		 JListAllDepts.setBounds(403, 37, 377, 200);
-		//scrollPanel_in_JlistAllDepts.setViewportView(JListAllDepts);
-				
-		
-		ActionListener showAllDepartamentosActionListener =new ActionListener() {
+		JListAllDepts = new JList();
+		panel.add(JListAllDepts);
+
+		JListAllDepts.setBounds(403, 37, 377, 200);
+
+		JButton btnCrearNuevoDepartamento = new JButton("Crear nuevo departamento");
+		btnCrearNuevoDepartamento.addActionListener(new ActionListener() {
+			
 			public void actionPerformed(ActionEvent e) {
-				List<Departamento> departamentos =departamentoServicio.getAll();
+
+				  JFrame owner = (JFrame) SwingUtilities.getRoot((Component)e.getSource());
+				   CreateNewDeptDialog createDialog = new CreateNewDeptDialog(owner,Dialog.ModalityType.DOCUMENT_MODAL);
+				   createDialog.setVisible(true);
+				
+			}
+		});
+		btnCrearNuevoDepartamento.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnCrearNuevoDepartamento.setBounds(50, 85, 208, 36);
+		panel.add(btnCrearNuevoDepartamento);
+		// scrollPanel_in_JlistAllDepts.setViewportView(JListAllDepts);
+
+		ActionListener showAllDepartamentosActionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<Departamento> departamentos = departamentoServicio.getAll();
 				DefaultListModel<Departamento> defModel = new DefaultListModel<>();
 
 				defModel.addAll(departamentos);
-				int jlist_id=-1;
-//				for(int i=0; i<frame.getContentPane().getComponentCount(); i++) {
-//					if(frame.getContentPane().getComponent(i) instanceof JList) {
-//						jlist_id=i;
-//						break;
-//					}
-//				}
-//				if(jlist_id!=-1) {
-//					frame.getContentPane().remove(jlist_id);
-//				}
-				//JList<Departamento> modulos_list = new JList<Modulo>(defModel);
-//				JListAllDepts.setBounds(frame.getContentPane().getWidth() / 2, 0, frame.getContentPane().getWidth() / 2,
-//						frame.getContentPane().getHeight() / 2);
-//				frame.getContentPane().add(modulos_list);
 				
 
-				//frame.getContentPane().repaint(); 
-					JListAllDepts.setModel(defModel);
-				//JListAllDepts.setSize(new Dimension(scrollPane.getWidth(),scrollPane_1.getHeight()));
-					addMensaje(true, "Se han recuperado: " +departamentos.size() + " departamentos");
+				JListAllDepts.setModel(defModel);
+				addMensaje(true, "Se han recuperado: " + departamentos.size() + " departamentos");
 			}
 		};
 		btnShowAllDepts.addActionListener(showAllDepartamentosActionListener);
 
-
 	}
-
-
 
 	private void addMensaje(boolean keepText, String msg) {
 		String oldText = "";
